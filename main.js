11a8682365caae54de6a4f6ba34236acd7a0e02c
Alyana Imperial <allyimperial@gmail.com>
2026-01-12 15:38:16 -0600

    created exit button, new file preload.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,7 +11,8 @@ function createWindow() {
     frame: false, 
     transparent: false,
     webPreferences: {
-      contextIsolation: true
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js")
     }
   });
 
@@ -21,4 +23,8 @@ app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("close-app", () => {
+  app.quit();
 });
