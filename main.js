ca582c742629c6e2664c0dda828a19e418feaf0c
Alyana Imperial <allyimperial@gmail.com>
2026-01-15 13:15:58 -0600

    main.js edited, exit button works again

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,10 +8,11 @@ function createWindow() {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    frame: false, 
+    frame: false,
     transparent: false,
     webPreferences: {
-      contextIsolation: true
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js")
     }
   });
 
@@ -21,4 +23,8 @@ app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("close-app", () => {
+  app.quit();
 });
